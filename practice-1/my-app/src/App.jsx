105f31171fcb3539977bae96dd9a30d919a77ed8
Ritik Thakur <64410199+riki8055@users.noreply.github.com>
2026-03-10 07:00:26 +0000

    practice HOC (Higher Order Component)

--- practice-1/my-app/src/App.jsx
+++ practice-1/my-app/src/App.jsx
@@ -3,11 +3,12 @@ import RegistrationForm from "./components/RegistrationForm";
 import MyBrowserRouter from "./components/BrowserRouter";
 import UseTransitionEx from "./components/UseTransitionEx";
 import ForwardRefEx from "./components/ForwardRefEx";
+import HOCEx from "./components/HOCEx";
 
 function App() {
   return (
     <>
-      <ForwardRefEx />
+      <HOCEx />
     </>
   );
 }
