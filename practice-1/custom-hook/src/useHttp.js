601d7732e73dd6a8e75c479e58995ace0e5a4f5e
Ritik Thakur <thakurritik8055@gmail.com>
2026-04-01 07:33:52 +0530

    create a custom hook for http

---
+++ practice-1/custom-hook/src/useHttp.js
@@ -0,0 +1,25 @@
+export async function fetchPokemons() {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch pokemons!");
+    }
+
+    for (const pokemon of data.results) {
+      const res = await fetch(pokemon.url);
+      const details = await res.json();
+
+      console.log({
+        name: details.name,
+        height: details.height,
+        weight: details.weight,
+      });
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
