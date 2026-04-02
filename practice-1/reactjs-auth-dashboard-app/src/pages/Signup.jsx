35382f3363f53954199a18c73b8f5f538756d56a
Ritik Thakur <thakurritik8055@gmail.com>
2026-04-02 21:14:29 +0530

    control signup form inputs

--- practice-1/reactjs-auth-dashboard-app/src/pages/Signup.jsx
+++ practice-1/reactjs-auth-dashboard-app/src/pages/Signup.jsx
@@ -8,30 +8,51 @@ function Signup() {
     password: "",
   });
 
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(formData);
+  }
+
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>Signup</h2>
 
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           className={styles.input}
           type="text"
           placeholder="Name"
+          name="name"
           value={formData.name}
+          onChange={handleChange}
         />
 
         <input
           className={styles.input}
           type="email"
           placeholder="Email"
+          name="email"
           value={formData.email}
+          onChange={handleChange}
         />
 
         <input
           className={styles.input}
           type="password"
           placeholder="Password"
+          name="password"
           value={formData.password}
+          onChange={handleChange}
         />
 
         <button className={styles.button} type="submit">
